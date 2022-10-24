e286845463487a1275474063def2b3d615c88326
Ompio <72317376+Ompio@users.noreply.github.com>
2022-10-25 00:01:01 +0200

    update

--- Color_Cutting_Sol/Color_Cutting_App/Color_Cutting_App.cpp
+++ Color_Cutting_Sol/Color_Cutting_App/Color_Cutting_App.cpp
@@ -2,8 +2,52 @@
 #include <windows.h> 
 #include <stdio.h> 
 #include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 typedef int(__cdecl* MYPROC)(void); //definicja typu wskaznika
+typedef unsigned char byte;
+
+
+struct BMPFile {
+    int height;
+    int width;
+    const char* filename;
+    byte* pixels;
+    byte* data;
+
+    BMPFile(const char* filename) {
+        //FILE* f = fopen(filename, "rb");
+        this->filename = filename;
+        fstream f("k.bmp", ios::binary || ios::in);
+        if (f) {
+            data = new byte[54];
+            f.read((char*)data, 54);
+
+
+
+            // extract image height and width from header
+            memcpy(&width, data + 18, sizeof(int));
+            memcpy(&height, data + 22, sizeof(int));
+
+            int size = ((width * 3 + 3) & (~3)) * height; //padding
+            pixels = new byte[size];
+            f.read((char*)pixels, size);
+        }
+        else {
+            cout << "we got nothing";
+        }
+
+        f.close();
+        //unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
+
+        //fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
+
+        //fclose(f);
+    }
+};
 
 int main(void)
 {
@@ -62,6 +106,21 @@ int main(void)
         printf("Message printed from executable\n");
     if (!fRunTimeLinkSuccessC)
         printf("C Message printed from executable\n");
+
+    //BMP and threads
+    BMPFile test("k.bmp");
+    /*
+    std::fstream plik;
+    plik.open("omg.txt", ios::in);
+  
+    if (plik) {
+        cout << "heklkp";
+    }*/
+    //Loading BMP
+    //Threads to DLL's
+
+    //Results to output
+    //plik.close();
 
     return 0;
 
